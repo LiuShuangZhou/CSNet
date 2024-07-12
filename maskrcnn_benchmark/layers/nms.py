85d67f338636ac8b4eefead2faec799b99029c09
LiuShuangZhou <liushuzngzhou22@mails.ucas.ac.cn>
2024-07-12 18:51:14 +0800

    Add files via upload

---
+++ maskrcnn_benchmark/layers/nms.py
@@ -0,0 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# from ._utils import _C
+# from maskrcnn_benchmark import _C
+from torchvision.ops import nms
+nms = nms
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
